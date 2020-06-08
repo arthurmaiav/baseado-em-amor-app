a8021d8a7bfacc9c75d60a415df9081e6334fe7f
arthurmaiav <arthurmaiav@gmail.com>
2020-06-08 19:57:46 -0300

    Ajuste no modal de contas

--- src/pages/ModalBankAccount/ModalBankAccount.js
+++ src/pages/ModalBankAccount/ModalBankAccount.js
@@ -23,17 +23,27 @@ export default class ModalTester extends Component {
 
                         <View style={styles.sectionInfo}>
                             <Text style={styles.sectionTitle}>Banco:</Text>
-                            <Text style={styles.sectionDescription}>Itaú</Text>
+                            <Text style={styles.sectionDescription}>Caixa (Poupança)</Text>
                         </View>
 
                         <View style={styles.sectionInfo}>
-                            <Text style={styles.sectionTitle}>Agencia:</Text>
-                            <Text style={styles.sectionDescription}>1189</Text>
+                            <Text style={styles.sectionTitle}>Operação:</Text>
+                            <Text style={styles.sectionDescription}>013</Text>
+                        </View>
+
+                        <View style={styles.sectionInfo}>
+                            <Text style={styles.sectionTitle}>Agência:</Text>
+                            <Text style={styles.sectionDescription}>0472</Text>
                         </View>
 
                         <View style={styles.sectionInfo}>
                             <Text style={styles.sectionTitle}>Conta:</Text>
-                            <Text style={styles.sectionDescription}>123456-8</Text>
+                            <Text style={styles.sectionDescription}>8021-6</Text>
+                        </View>
+
+                        <View style={styles.sectionInfo}>
+                            <Text style={styles.sectionTitle}>CPF:</Text>
+                            <Text style={styles.sectionDescription}>521.782.770-04</Text>
                         </View>
 
                         <View style={styles.actions}>
